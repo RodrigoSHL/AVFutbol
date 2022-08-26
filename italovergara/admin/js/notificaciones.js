258a2677ef6a783b8535e86e7e783b3ad60a792a
RodrigoSHL <shl.catalan@gmail.com>
2022-08-26 08:59:20 -0400

    fix: delete unnecesary files
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- italovergara/admin/js/notificaciones.js
+++
@@ -1,9 +0,0 @@
-function mostrar_notificacion(type, text, class_name, position){
-    position = (position) ? position : 'bottom-left';
-    $.gritter.add({
-        title: type,
-        text: text,
-        class_name: class_name,
-        position: position
-    });
-};
